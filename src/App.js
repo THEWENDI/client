36380a87607c96b3c473952a48e82351dd2f5fac
Wendi Li <issacbbb@hotmaill.com>
2022-03-23 12:30:50 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom"
 import './App.css';
+import Main from "./views/Main"
+import EditProduct from "./views/EditProduct"
+import OneProduct from "./views/OneProduct"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main/>
+          </Route>
+          <Route exact path="/products/:id">
+            <OneProduct/>
+          </Route>
+          <Route exact path="/products/:id/edit">
+            <EditProduct/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
